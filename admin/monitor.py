ced47594526fd5655635a6d87411007a1e302963
Shurui Zhou <shuruiz@cs.cmu.edu>
2020-01-20 15:42:17 -0500

    fix str

--- admin/monitor.py
+++ admin/monitor.py
@@ -72,122 +72,122 @@ def updateResult():
     else:
         path = '/Users/shuruiz/Work/ForkData/INTRUDE'
         # for every file (repository) in the dupPR directory
-    for dir_name in os.listdir(path):
-
-        # print(dir_name)
-        # find path to this file
-        filepath = path
-        if platform.system() == 'Windows':
-            filepath += '\\'
-        else:
-            filepath += '/'
-        filepath += dir_name
-        if not os.path.isdir(filepath):
-            # print("not dir " + filepath)
-            continue
-        record_date = datetime.strptime(dir_name, '%Y-%m-%d')
-        if ((datetime.now() - record_date).days > 2):
-            # print(str(record_date) + "is older than 2 days, skip")
-            continue
-
-        for repoPRlist in os.listdir(filepath):
-            # print(repoPRlist)
-            repo_filepath = filepath + "/" + repoPRlist
-            # open this file
-            with open(repo_filepath) as tsv:
-                # for every line (PR pair) in the current file
-                for line in csv.reader(tsv, delimiter="\t"):
-                    repoURL = line[0]
-                    PR1 = line[1]
-                    PR2 = line[3]
-                    # check whether this pr pair has already been added to the db:
-                    flag = 0
-                    # searchCurrentPRPari_sql = "SELECT * FROM duppr_pair where repo = '" + repoURL + "' and PR1 = " + PR1 + " AND PR2 = " + PR2
-                    searchCurrentPRPari_sql = "SELECT * FROM duppr_pair_update where repo = '" + repoURL + "' and PR1 = " + PR1 + " AND PR2 = " + PR2
-                    # print(searchCurrentPRPari_sql)
-                    cur.execute(searchCurrentPRPari_sql)
-                    check = cur.fetchall()
-                    if (len(check) > 0):
-                        flag = 1
-
-                    if (flag == 0):
-                        pr_pair_tuple = (
-                            repoURL, int(PR1, 10), int(PR2, 10), float(line[similarity_final]), float(line[title_lsi]), \
-                            float(line[title_tfidf]), float(line[body_lsi]), float(line[body_tfidf]),
-                            float(line[commit_lsi]), float(line[commit_tfidf]), \
-                            float(line[pr1_add_files]), float(line[pr2_add_files]), float(line[pr1_delete_files]),
-                            float(line[pr2_delete_files]), float(line[add_files_sim]), float(line[delet_files_sim]),
-                            float(line[add_filenames_sim]), float(line[delet_filenames_sim]), \
-                            float(line[code_sim_addCode_lsi]), float(line[code_sim_addCode_lsi_overlap_filepath]),
-                            float(line[code_sim_addCode_lsi_same_filename]), float(line[code_sim_addCode_tfidf_add]),
-                            float(line[code_sim_addCode_tfidf_overlap_filepath]), \
-                            float(line[code_sim_addCode_tfidf_same_filename]), float(line[code_sim_deleteCode_lsi]),
-                            float(line[code_sim_deleteCode_lsi_overlap_filepath]),
-                            float(line[code_sim_deleteCodelsi_same_filename]),
-                            float(line[code_sim_deleteCode_tfidf_add]), \
-                            float(line[code_sim_deleteCode_tfidf_overlap_filepath]),
-                            float(line[code_sim_deleteCodetfidf_same_filename]),
-                            float(line[location_similarity_allfile]), float(line[location_similarity_allfile]),
-                            float(line[timeInterval]), \
-                            float(line[ref_version]), float(line[ref_issue]), float(line[ref_SHA]),
-                            float(line[ref_url]), line[pr2_created_at_index])
-                        cur.execute('INSERT INTO duppr_pair_update(repo, pr1, pr2, score, title_lsi,\
-                                                                       title_tfidf, body_lsi, body_tfidf, commit_lsi, commit_tfidf,\
-                                                                       pr1_add_files, pr2_add_files, pr1_delete_files, pr2_delete_files, add_files_sim,\
-                                                                       delet_files_sim, add_filenames_sim, delet_filenames_sim, code_sim_addCode_lsi, code_sim_addCode_lsi_overlap_filepath,\
-                                                                       code_sim_addCode_lsi_same_filename, code_sim_addCode_tfidf_add, code_sim_addCode_tfidf_overlap_filepath, code_sim_addCode_tfidf_same_filename, code_sim_deleteCode_lsi,\
-                                                                       code_sim_deleteCode_lsi_overlap_filepath, code_sim_deleteCodelsi_same_filename, code_sim_deleteCode_tfidf_add, code_sim_deleteCode_tfidf_overlap_filepath, code_sim_deleteCodetfidf_same_filename, \
-                                                                       location_similarity_allfile, location_similarity_overlapfile, timeInterval, ref_version, ref_issue,\
-                                                                        ref_SHA, ref_url, timestamp) \
-                                                      VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
-                                                              %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
-                                                              %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
-                                                              %s,%s,%s,%s,%s,%s,%s,%s)',
-                                    pr_pair_tuple)
-
-                    else:
-                        pr_pair_update_tuple = (
-                            float(line[similarity_final]), float(line[title_lsi]), \
-                            float(line[title_tfidf]), float(line[body_lsi]), float(line[body_tfidf]),
-                            float(line[commit_lsi]), float(line[commit_tfidf]), \
-                            float(line[pr1_add_files]), float(line[pr2_add_files]), float(line[pr1_delete_files]),
-                            float(line[pr2_delete_files]), float(line[add_files_sim]), \
-                            float(line[delet_files_sim]), float(line[add_filenames_sim]),
-                            float(line[delet_filenames_sim]), float(line[code_sim_addCode_lsi]),
-                            float(line[code_sim_addCode_lsi_overlap_filepath]), \
-                            float(line[code_sim_addCode_lsi_same_filename]), float(line[code_sim_addCode_tfidf_add]),
-                            float(line[code_sim_addCode_tfidf_overlap_filepath]),
-                            float(line[code_sim_addCode_tfidf_same_filename]), float(line[code_sim_deleteCode_lsi]), \
-                            float(line[code_sim_deleteCode_lsi_overlap_filepath]),
-                            float(line[code_sim_deleteCodelsi_same_filename]),
-                            float(line[code_sim_deleteCode_tfidf_add]),
-                            float(line[code_sim_deleteCode_tfidf_overlap_filepath]),
-                            float(line[code_sim_deleteCodetfidf_same_filename]), \
-                            float(line[location_similarity_allfile]), float(line[location_similarity_allfile]),
-                            float(line[timeInterval]), float(line[ref_version]), float(line[ref_issue]), \
-                            float(line[ref_SHA]), float(line[ref_url]), line[pr2_created_at_index], repoURL,
-                            int(PR1, 10), int(PR2, 10))
-
-                        # update_sql = "UPDATE duppr_pair \
-                        #               set score = %s, title = %s, description = %s, patch_content = %s, patch_content_overlap = %s, \
-                        #                   changed_file = %s, changed_file_overlap = %s, location = %s, location_overlap = %s, issue_number = %s, \
-                        #                   commit_message= %s, timestamp = %s  \
-                        #               where repo = %s and pr1 = %s and pr2 = %s"
-                        update_sql = "UPDATE duppr_pair_update \
-                                         set score = %s, title_lsi = %s, \
-                                         title_tfidf = %s, body_lsi = %s, body_tfidf = %s, commit_lsi = %s, commit_tfidf = %s, \
-                                         pr1_add_files = %s, pr2_add_files = %s, pr1_delete_files = %s, pr2_delete_files = %s, add_files_sim = %s, \
-                                         delet_files_sim = %s, add_filenames_sim = %s, delet_filenames_sim = %s, code_sim_addCode_lsi = %s, code_sim_addCode_lsi_overlap_filepath = %s, \
-                                         code_sim_addCode_lsi_same_filename = %s, code_sim_addCode_tfidf_add = %s, code_sim_addCode_tfidf_overlap_filepath = %s, code_sim_addCode_tfidf_same_filename = %s, code_sim_deleteCode_lsi = %s,\
-                                         code_sim_deleteCode_lsi_overlap_filepath = %s,  code_sim_deleteCodelsi_same_filename = %s, code_sim_deleteCode_tfidf_add = %s, code_sim_deleteCode_tfidf_overlap_filepath = %s, code_sim_deleteCodetfidf_same_filename = %s,\
-                                         location_similarity_allfile = %s, location_similarity_overlapfile = %s, timeInterval = %s, ref_version = %s, ref_issue = %s,\
-                                         ref_SHA = %s, ref_url = %s, timestamp = %s   \
-                                         where repo = %s and pr1 = %s and pr2 = %s"
-                        cur.execute(update_sql, pr_pair_update_tuple)
-
-                    conn.commit()
-    print('update pr states...')
-    updatePRstate()
+    # for dir_name in os.listdir(path):
+    #
+    #     # print(dir_name)
+    #     # find path to this file
+    #     filepath = path
+    #     if platform.system() == 'Windows':
+    #         filepath += '\\'
+    #     else:
+    #         filepath += '/'
+    #     filepath += dir_name
+    #     if not os.path.isdir(filepath):
+    #         # print("not dir " + filepath)
+    #         continue
+    #     record_date = datetime.strptime(dir_name, '%Y-%m-%d')
+    #     if ((datetime.now() - record_date).days > 2):
+    #         # print(str(record_date) + "is older than 2 days, skip")
+    #         continue
+    #
+    #     for repoPRlist in os.listdir(filepath):
+    #         # print(repoPRlist)
+    #         repo_filepath = filepath + "/" + repoPRlist
+    #         # open this file
+    #         with open(repo_filepath) as tsv:
+    #             # for every line (PR pair) in the current file
+    #             for line in csv.reader(tsv, delimiter="\t"):
+    #                 repoURL = line[0]
+    #                 PR1 = line[1]
+    #                 PR2 = line[3]
+    #                 # check whether this pr pair has already been added to the db:
+    #                 flag = 0
+    #                 # searchCurrentPRPari_sql = "SELECT * FROM duppr_pair where repo = '" + repoURL + "' and PR1 = " + PR1 + " AND PR2 = " + PR2
+    #                 searchCurrentPRPari_sql = "SELECT * FROM duppr_pair_update where repo = '" + repoURL + "' and PR1 = " + PR1 + " AND PR2 = " + PR2
+    #                 # print(searchCurrentPRPari_sql)
+    #                 cur.execute(searchCurrentPRPari_sql)
+    #                 check = cur.fetchall()
+    #                 if (len(check) > 0):
+    #                     flag = 1
+    #
+    #                 if (flag == 0):
+    #                     pr_pair_tuple = (
+    #                         repoURL, int(PR1, 10), int(PR2, 10), float(line[similarity_final]), float(line[title_lsi]), \
+    #                         float(line[title_tfidf]), float(line[body_lsi]), float(line[body_tfidf]),
+    #                         float(line[commit_lsi]), float(line[commit_tfidf]), \
+    #                         float(line[pr1_add_files]), float(line[pr2_add_files]), float(line[pr1_delete_files]),
+    #                         float(line[pr2_delete_files]), float(line[add_files_sim]), float(line[delet_files_sim]),
+    #                         float(line[add_filenames_sim]), float(line[delet_filenames_sim]), \
+    #                         float(line[code_sim_addCode_lsi]), float(line[code_sim_addCode_lsi_overlap_filepath]),
+    #                         float(line[code_sim_addCode_lsi_same_filename]), float(line[code_sim_addCode_tfidf_add]),
+    #                         float(line[code_sim_addCode_tfidf_overlap_filepath]), \
+    #                         float(line[code_sim_addCode_tfidf_same_filename]), float(line[code_sim_deleteCode_lsi]),
+    #                         float(line[code_sim_deleteCode_lsi_overlap_filepath]),
+    #                         float(line[code_sim_deleteCodelsi_same_filename]),
+    #                         float(line[code_sim_deleteCode_tfidf_add]), \
+    #                         float(line[code_sim_deleteCode_tfidf_overlap_filepath]),
+    #                         float(line[code_sim_deleteCodetfidf_same_filename]),
+    #                         float(line[location_similarity_allfile]), float(line[location_similarity_allfile]),
+    #                         float(line[timeInterval]), \
+    #                         float(line[ref_version]), float(line[ref_issue]), float(line[ref_SHA]),
+    #                         float(line[ref_url]), line[pr2_created_at_index])
+    #                     cur.execute('INSERT INTO duppr_pair_update(repo, pr1, pr2, score, title_lsi,\
+    #                                                                    title_tfidf, body_lsi, body_tfidf, commit_lsi, commit_tfidf,\
+    #                                                                    pr1_add_files, pr2_add_files, pr1_delete_files, pr2_delete_files, add_files_sim,\
+    #                                                                    delet_files_sim, add_filenames_sim, delet_filenames_sim, code_sim_addCode_lsi, code_sim_addCode_lsi_overlap_filepath,\
+    #                                                                    code_sim_addCode_lsi_same_filename, code_sim_addCode_tfidf_add, code_sim_addCode_tfidf_overlap_filepath, code_sim_addCode_tfidf_same_filename, code_sim_deleteCode_lsi,\
+    #                                                                    code_sim_deleteCode_lsi_overlap_filepath, code_sim_deleteCodelsi_same_filename, code_sim_deleteCode_tfidf_add, code_sim_deleteCode_tfidf_overlap_filepath, code_sim_deleteCodetfidf_same_filename, \
+    #                                                                    location_similarity_allfile, location_similarity_overlapfile, timeInterval, ref_version, ref_issue,\
+    #                                                                     ref_SHA, ref_url, timestamp) \
+    #                                                   VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
+    #                                                           %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
+    #                                                           %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,\
+    #                                                           %s,%s,%s,%s,%s,%s,%s,%s)',
+    #                                 pr_pair_tuple)
+    #
+    #                 else:
+    #                     pr_pair_update_tuple = (
+    #                         float(line[similarity_final]), float(line[title_lsi]), \
+    #                         float(line[title_tfidf]), float(line[body_lsi]), float(line[body_tfidf]),
+    #                         float(line[commit_lsi]), float(line[commit_tfidf]), \
+    #                         float(line[pr1_add_files]), float(line[pr2_add_files]), float(line[pr1_delete_files]),
+    #                         float(line[pr2_delete_files]), float(line[add_files_sim]), \
+    #                         float(line[delet_files_sim]), float(line[add_filenames_sim]),
+    #                         float(line[delet_filenames_sim]), float(line[code_sim_addCode_lsi]),
+    #                         float(line[code_sim_addCode_lsi_overlap_filepath]), \
+    #                         float(line[code_sim_addCode_lsi_same_filename]), float(line[code_sim_addCode_tfidf_add]),
+    #                         float(line[code_sim_addCode_tfidf_overlap_filepath]),
+    #                         float(line[code_sim_addCode_tfidf_same_filename]), float(line[code_sim_deleteCode_lsi]), \
+    #                         float(line[code_sim_deleteCode_lsi_overlap_filepath]),
+    #                         float(line[code_sim_deleteCodelsi_same_filename]),
+    #                         float(line[code_sim_deleteCode_tfidf_add]),
+    #                         float(line[code_sim_deleteCode_tfidf_overlap_filepath]),
+    #                         float(line[code_sim_deleteCodetfidf_same_filename]), \
+    #                         float(line[location_similarity_allfile]), float(line[location_similarity_allfile]),
+    #                         float(line[timeInterval]), float(line[ref_version]), float(line[ref_issue]), \
+    #                         float(line[ref_SHA]), float(line[ref_url]), line[pr2_created_at_index], repoURL,
+    #                         int(PR1, 10), int(PR2, 10))
+    #
+    #                     # update_sql = "UPDATE duppr_pair \
+    #                     #               set score = %s, title = %s, description = %s, patch_content = %s, patch_content_overlap = %s, \
+    #                     #                   changed_file = %s, changed_file_overlap = %s, location = %s, location_overlap = %s, issue_number = %s, \
+    #                     #                   commit_message= %s, timestamp = %s  \
+    #                     #               where repo = %s and pr1 = %s and pr2 = %s"
+    #                     update_sql = "UPDATE duppr_pair_update \
+    #                                      set score = %s, title_lsi = %s, \
+    #                                      title_tfidf = %s, body_lsi = %s, body_tfidf = %s, commit_lsi = %s, commit_tfidf = %s, \
+    #                                      pr1_add_files = %s, pr2_add_files = %s, pr1_delete_files = %s, pr2_delete_files = %s, add_files_sim = %s, \
+    #                                      delet_files_sim = %s, add_filenames_sim = %s, delet_filenames_sim = %s, code_sim_addCode_lsi = %s, code_sim_addCode_lsi_overlap_filepath = %s, \
+    #                                      code_sim_addCode_lsi_same_filename = %s, code_sim_addCode_tfidf_add = %s, code_sim_addCode_tfidf_overlap_filepath = %s, code_sim_addCode_tfidf_same_filename = %s, code_sim_deleteCode_lsi = %s,\
+    #                                      code_sim_deleteCode_lsi_overlap_filepath = %s,  code_sim_deleteCodelsi_same_filename = %s, code_sim_deleteCode_tfidf_add = %s, code_sim_deleteCode_tfidf_overlap_filepath = %s, code_sim_deleteCodetfidf_same_filename = %s,\
+    #                                      location_similarity_allfile = %s, location_similarity_overlapfile = %s, timeInterval = %s, ref_version = %s, ref_issue = %s,\
+    #                                      ref_SHA = %s, ref_url = %s, timestamp = %s   \
+    #                                      where repo = %s and pr1 = %s and pr2 = %s"
+    #                     cur.execute(update_sql, pr_pair_update_tuple)
+    #
+    #                 conn.commit()
+    # print('update pr states...')
+    # updatePRstate()
 
     threshold = '0.8'
     print("find pr pairs similar score is higher than "+ threshold)
@@ -200,7 +200,7 @@ def updateResult():
 
 
         for prPair in prPair_list:
-            pr_list += prPair[1] + " " + prPair[2]  + " " + prPair[3]  + " " + prPair[4] +" ; "
+            pr_list += str(prPair[1]) + " " + str(prPair[2])  + " " + str(prPair[3])  + " " +str(prPair[4]) +" ; "
             result_list.append(str(prPair[0]))
 
         idlist_str = "(" + ', '.join(map(str, result_list)) + ")"
